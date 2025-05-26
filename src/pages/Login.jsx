2cc7a1ad7f9fa0e0b7833cf4a214aa86dfebb694
Rizki Ramadhan <rizki@nadyne.net>
2025-05-26 17:25:33 +0700

    Update 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,72 @@ import { React } from 'react'
 
 function Login() {
     return (
-        <div>
-            <h1>Hello, this is a login page!</h1>
+        <div className="min-h-screen flex flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img className="mx-auto h-10 w-auto" src="/logo.png" alt="Your Company" />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+    
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    className="block w-full rounded-md border border-gray-300 px-3 py-1.5 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                  />
+                </div>
+              </div>
+    
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    className="block w-full rounded-md border border-gray-300 px-3 py-1.5 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm"
+                  />
+                </div>
+              </div>
+    
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+    
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Not a member?{' '}
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Start a 14 day free trial
+              </a>
+            </p>
+          </div>
         </div>
-    )
+      );
 }
 
 export default Login;
