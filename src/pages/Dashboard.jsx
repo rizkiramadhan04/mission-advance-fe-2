120c3bd9e53045458d3b67b3f1102b7aced722af
Rizki Ramadhan <rizki@nadyne.net>
2025-05-26 16:47:58 +0700

    First Commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { React } from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Hello, this is a dashboard page!</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
