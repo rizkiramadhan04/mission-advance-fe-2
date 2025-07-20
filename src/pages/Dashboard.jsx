0daf813b7be96b075a9201ea2dc02f1d45e69a36
ramadhan <ramadhan@users-MacBook-Pro.local>
2025-07-21 06:07:40 +0700

    Update terbaru

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,29 @@
 // src/pages/Dashboard.js
 import React, { useState, useEffect } from 'react';
+import Logo from '../assets/images/Logo.png';
+import UserLogo from '../assets/images/user.png';
+import AngelDown from '../assets/images/angel-down.png';
+import ImageNetFilm1 from '../assets/images/next-film/next-film-1.png';
+import ImageNetFilm2 from '../assets/images/next-film/next-film-2.png';
+import ImageNetFilm3 from '../assets/images/next-film/next-film-3.png';
+import ImageNetFilm4 from '../assets/images/next-film/next-film-4.png';
+import ImageRatingFilm1 from '../assets/images/rating-film/rating-film-1.png';
+import ImageRatingFilm2 from '../assets/images/rating-film/rating-film-2.png';
+import ImageRatingFilm3 from '../assets/images/rating-film/rating-film-3.png';
+import ImageRatingFilm4 from '../assets/images/rating-film/rating-film-4.png';
+import ImageRatingFilm5 from '../assets/images/rating-film/rating-film-5.png';
+import ImageTopFilm1 from '../assets/images/top-film/top-film-1.png';
+import ImageTopFilm2 from '../assets/images/top-film/top-film-2.png';
+import ImageTopFilm3 from '../assets/images/top-film/top-film-3.png';
+import ImageTopFilm4 from '../assets/images/top-film/top-film-4.png';
+import ImageTopFilm5 from '../assets/images/top-film/top-film-5.png';
+import ImageNewFilm1 from '../assets/images/new-film/new-film-1.png';
+import ImageNewFilm2 from '../assets/images/new-film/new-film-2.png';
+import ImageNewFilm3 from '../assets/images/new-film/new-film-3.png';
+import ImageNewFilm4 from '../assets/images/new-film/new-film-4.png';
+import ImageNewFilm5 from '../assets/images/new-film/new-film-5.png';
+import ImagHero from '../assets/images/hero_image.png';
+
 
 function Dashboard() {
   const [users, setUsers] = useState([]);
@@ -25,33 +49,160 @@ function Dashboard() {
   };
 
   return (
-    <div className="min-h-screen max-w-screen items-center justify-center bg-[#161622] text-white p-4">
-      <h3 className="text-xl font-bold mb-4">Daftar User</h3>
-      <a href="/register" className='text-2xl text-white font-bold'>Add New Profile</a>
-      <table className='table-auto w-full text-center text-sm border border-white'>
-        <thead>
-          <tr className='border-b border-white'>
-            <th className='border-white border-r'>No</th>
-            <th className='border-white border-r'>Nama</th>
-            <th className='border-white border-r'>Email</th>
-            <th className='border-white border-r'>Aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={index} className='border-b border-white'>
-              <td className='border-r border-white'>{index + 1}</td>
-              <td className='border-r border-white'>{user.name}</td>
-              <td className='border-r border-white'>{user.email}</td>
-              <td className='space-x-2'>
-                <button onClick={() => handleEdit(index)} className="bg-blue-500 px-2 py-1 rounded">Edit</button>
-                <button onClick={() => handleDelete(index)} className="bg-red-500 px-2 py-1 rounded">Hapus</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+    <header className="bg-[#22282A] fixed w-full top-0 z-50">
+        <nav className="container mx-auto flex items-center justify-between p-4 lg:px-8">
+            <div className="hidden lg:flex items-center space-x-8">
+                <a href="#" className="left-1/2 transform -translate-x-1/2 flex items-center">
+                    <img src={Logo} className="h-8" alt="Logo" />
+                </a>
+                <a href="#" className="text-sm font-semibold text-white">Series</a>
+                <a href="#" className="text-sm font-semibold text-white">Film</a>
+                <a href="#" className="text-sm font-semibold text-white">Daftar Saya</a>
+            </div>
+            <div className="hidden lg:flex items-center space-x-3">
+                <img src={UserLogo} className="w-7 rounded-full" />
+                <img src={AngelDown} className="w-3" />
+            </div>
+            <button id="menu-toggle" className="lg:hidden text-white focus:outline-none">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+            </button>
+        </nav>
+        <div id="mobile-menu" className="hidden lg:hidden bg-[#22282A] absolute w-full mt-1 p-4">
+            <a href="#" className="block py-2 text-sm font-semibold">Series</a>
+            <a href="#" className="block py-2 text-sm font-semibold">Film</a>
+            <a href="#" className="block py-2 text-sm font-semibold">Daftar Saya</a>
+            <hr className="border-gray-600 my-2" />
+            <a href="#" className="flex items-center space-x-3 py-2">
+                <img src="./assets/images/user.png" className="w-7 rounded-full" />
+                <span>Profil</span>
+            </a>
+        </div>
+    </header>
+
+    <section className="relative bg-cover bg-center h-screen flex items-center" 
+        style={{ backgroundImage: `url(${ImagHero})` }}>
+        <div className="container mx-auto px-6 lg:px-20 text-white">
+            <div className="max-w-xl">
+                <h1 className="text-4xl font-semibold mb-4">Duty After School</h1>
+                <p className="mb-6">
+                    Sebuah benda tak dikenal mengambil alih dunia. Dalam keputusasaan, Departemen Pertahanan mulai merekrut lebih banyak tentara, termasuk siswa sekolah menengah.
+                </p>
+                <div className="flex space-x-4">
+                    <button className="px-6 py-2 bg-[#0F1E93] rounded-full">Mulai</button>
+                    <button className="px-6 py-2 bg-gray-700/50 rounded-full">Selengkapnya!</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section className="container mx-auto px-6 mt-12">
+        <h2 className="text-2xl font-bold mb-4">Melanjutkan Nonton Film</h2>
+        <div className="flex space-x-4 overflow-x-scroll scrollbar-hide">
+            <img src={ImageNetFilm1} className="w-74 rounded-lg" />
+            <img src={ImageNetFilm2} className="w-74 rounded-lg" />
+            <img src={ImageNetFilm3} className="w-74 rounded-lg" />
+            <img src={ImageNetFilm4} className="w-74 rounded-lg" />
+        </div>
+    </section>
+    
+    <section className="container mx-auto px-6 mt-12">
+        <h2 className="text-2xl font-bold mb-4">Top Rating Film dan Series Hari ini</h2>
+        <div className="flex space-x-4 overflow-x-scroll scrollbar-hide gap-4">
+            <img src={ImageRatingFilm1} className="w-55 rounded-lg" />
+            <img src={ImageRatingFilm2} className="w-55 rounded-lg" />
+            <img src={ImageRatingFilm3} className="w-55 rounded-lg" />
+            <img src={ImageRatingFilm4} className="w-55 rounded-lg" />
+            <img src={ImageRatingFilm5} className="w-55 rounded-lg" />
+        </div>
+    </section>
+    
+    <section className="container mx-auto px-6 mt-12">
+        <h2 className="text-2xl font-bold mb-4">Film Tranding</h2>
+        <div className="flex space-x-4 overflow-x-scroll scrollbar-hide gap-4">
+            <img src={ImageTopFilm1} className="w-55 rounded-lg" />
+            <img src={ImageTopFilm2} className="w-55 rounded-lg" />
+            <img src={ImageTopFilm3} className="w-55 rounded-lg" />
+            <img src={ImageTopFilm4} className="w-55 rounded-lg" />
+            <img src={ImageTopFilm5} className="w-55 rounded-lg" />
+        </div>
+    </section>
+
+    <section className="container mx-auto px-6 mt-12">
+        <h2 className="text-2xl font-bold mb-4">Film Tranding</h2>
+        <div className="flex space-x-4 overflow-x-scroll scrollbar-hide gap-4">
+            <img src={ImageNewFilm1} className="w-55 rounded-lg" />
+            <img src={ImageNewFilm2} className="w-55 rounded-lg" />
+            <img src={ImageNewFilm3} className="w-55 rounded-lg" />
+            <img src={ImageNewFilm4} className="w-55 rounded-lg" />
+            <img src={ImageNewFilm5} className="w-55 rounded-lg" />
+        </div>
+    </section>
+
+    
+    <footer className="bg-[#22282A] mt-16 py-10">
+        <div className="container mx-auto">
+            <div className="lg:flex lg:gap-6 text-white">
+                <div className="w-80">
+                    <div className="m-3">
+                        <img src={Logo} className="w-40" alt="img-logo" />
+                    </div>
+                    <div className="m-3 text-gray-400">
+                        <h1>@2023 Chill All Rights Reserved.</h1>
+                    </div>
+                </div>
+            
+                <div className="max-md:m-3 max-md:flex max-md:justify-between">
+                    <h1 className="font-bold text-lg mb-3">Genre</h1>
+                    <div className="flex justify-center min-lg:hidden">
+                        {/* <span>&rarr;</span> */}
+                    </div>
+                    <div className="grid grid-cols-4 gap-5 max-md:hidden">
+                        <div className="space-y-2">
+                            <a href="#" className="block text-gray-400 hover:text-white">Aksi</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Anak-anak</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Anime</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Britania</a>
+                        </div>
+                        <div className="space-y-2">
+                            <a href="#" className="block text-gray-400 hover:text-white">Drama</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Fantasi Ilmiah & Fantasi</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Kejahatan</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">KDrama</a>
+                        </div>
+                        <div className="space-y-2">
+                            <a href="#" className="block text-gray-400 hover:text-white">Komedi</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Petualangan</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Perang</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Romantis</a>
+                        </div>
+                        <div className="space-y-2">
+                            <a href="#" className="block text-gray-400 hover:text-white">Sains & Alam</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Thriller</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="max-md:m-3 max-md:m-3 max-md:flex max-md:justify-between">
+                    <h1 className="font-bold text-lg mb-3">Bantuan</h1>
+                    <div className="flex justify-center min-lg:hidden">
+                        {/* <span>&rarr;</span> */}
+                    </div>
+                    <div className="grid max-md:hidden">
+                        <div className="space-y-2">
+                            <a href="#" className="block text-gray-400 hover:text-white">FAQ</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Kontak Kami</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Privasi</a>
+                            <a href="#" className="block text-gray-400 hover:text-white">Syarat & Ketentuan</a>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </footer>
+    </>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,63 +1,67 @@
 import { React } from 'react'
 
+import Logo from '../assets/images/Logo.png';
+import GoogleLogo from '../assets/images/google.png';
+import BgImage from '../assets/images/image-login.jpeg';
+
 function Login() {
     return (
+      <div className="bg-cover" style={{ backgroundImage: `url(${BgImage})` }}>
+        <div className="flex items-center justify-center h-screen">
+          <section className="bg-black/90 w-1/3 h-auto mx-auto p-6 rounded-xl text-center">
+            <div className="grid grid-rows justify-items-center my-2.5">
+              <div className="icon">
+                <img src={Logo} className="w-35" alt="image-logo" />
+              </div>
+              <div className="head my-7">
+                <h1 className="text-3xl font-medium text-white">Masuk</h1>
+                <p className="text-sm text-white mt-2">Selamat datang kembali!</p>
+              </div>
+              <div className="body-form grid grid-rows gap-y-4">
 
-      <div className="min-h-screen max-w-screen flex items-center justify-center bg-gray-900">
-        <div className="bg-white rounded-lg shadow-lg px-6 py-12 w-full max-w-sm">
-          <div className="text-center">
-            {/* <img className="mx-auto h-10 w-auto" src="/logo.png" alt="Your Company" /> */}
-            <h2 className="mt-6 text-2xl font-bold text-gray-900">Sign in to your account</h2>
-          </div>
+                <div className="form-input-items text-white text-start">
+                  <label for="username" className="block text-sm/6 font-medium text-current">Username</label>
+                  <div className="mt-2">
+                    <div className="flex items-center rounded-full bg-transparent pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-white-600">
+                      <input type="text" name="username" id="username" className="block min-w-85 min-h-12 grow py-1.5 pr-3 pl-1 text-base text-white-900 placeholder:text-white-400 focus:outline-none sm:text-sm/6" placeholder="Masukan username" />
+                    </div>
+                  </div>
+                </div>
 
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email address
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              />
-            </div>
+                <div className="form-input-items text-white text-start">
+                  <label for="password" className="block text-sm/6 font-medium text-current">Kata Sandi</label>
+                  <div className="mt-2">
+                    <div className="flex items-center rounded-full bg-transparent pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-white-600">
+                      <input type="password" name="password" id="password" className="block min-w-85 min-h-12 grow py-1.5 pr-3 pl-1 text-base text-white-900 placeholder:text-white-400 focus:outline-none sm:text-sm/6" placeholder="Masukan kata sandi" />
+                    </div>
+                  </div>
+                  <div className="flex justify-between text-white mt-1">
+                    <small className="text-start">
+                      Belum punya akun? <a href="#" className="font-bold">Daftar</a>
+                    </small>
+                    <small className="text-end">
+                      <a href="#" className="font-bold">Lupa kata sandi?</a>
+                    </small>
+                  </div>
+                </div>
 
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              />
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot password?
-                </a>
+                <div>
+                  <button type="submit" className="flex w-full justify-center rounded-full bg-[#3D4142] px-3 py-2.5 text-sm/6 font-semibold text-white shadow-xs hover:cursor-pointer hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white-600">Masuk</button>
+                </div>
+                <small className="text-gray-400">Atau</small>
+                <div>
+                  <button type="submit" className="flex w-full justify-center rounded-full bg-transparent px-3 py-2.5 text-sm/6 font-semibold text-white shadow-xs outline-1 outline-[#E7E3FC] hover:cursor-pointer hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white-600">
+                    <img src={GoogleLogo} alt="google-icon" className="w-5 my-auto mr-5" />
+                    Masuk dengan google
+                  </button>
+                </div>
+                
               </div>
             </div>
-
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
+          </section>
         </div>
       </div>
+      
       
       );
 }
